87d6ced2ab4fa29ae3d928eac53e61ca48c57626
Chaeyong Chong <cychong@gmail.com>
2022-08-02 21:45:07 +0900

    add vim-style key binding for jumping to another window

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -9,7 +9,21 @@ local map = vim.api.nvim_set_keymap
 -- Toggle nvim-tree
 map('n', '<F3>', [[:NvimTreeToggle<CR>]], {})
 
+-- Toggle Tagbar
+map('n', '<F4>', [[:TagbarToggle<CR>]], {})
+
+-- Toggle show-number
+map('n', '<F5>', [[:set nonu<CR>]], {}) 
+
 -- Toggle more plugins
 --map('n', 'l', [[:IndentLinesToggle]], {})
 --map('n', 't', [[:TagbarToggle]], {})
 --map('n', 'ff', [[:Telescope find_files]], {})
+
+-- Move windows with JKLH
+map('n', '<C-j>', '<C-w>j', {})
+map('n', '<C-k>', '<C-w>k', {})
+map('n', '<C-l>', '<C-w>l', {})
+map('n', '<C-h>', '<C-w>h', {})
+
+
