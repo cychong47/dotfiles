0ed7f6bcefbeb7ba0a4ab3b3f11091df9de028a3
cychong47 <cychong@gmail.com>
2023-02-07 23:58:15 +0900

    install oh-my-zsh and powerline10k theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,1 +1,8 @@
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp ./zsh/zshrc ~/.zshrc
+
+# install powerline10k theme
+git clone https://github.com/romkatv/powerlevel10k.git /Users/$USER/.oh-my-zsh/themes/powerlevel10k
+
